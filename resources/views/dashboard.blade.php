772ab457d1fcd2c6fdfebdeeb69944735f052b1a
mhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-11-27 22:13:45 +0600

    dashboadr ui fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,55 +7,15 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <a href="{{route('contacts.index')}}">
+                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">All Contacts</button>
+            </a>
             <a href="{{route('contacts.create')}}">
                 <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add Contact</button>
             </a>
-
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
 
-<div class="overflow-x-auto relative">
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th scope="col" class="py-3 px-6">
-                    Name
-                </th>
-                <th scope="col" class="py-3 px-6">
-                    Mobile Number
-                </th>
-                <th scope="col" class="py-3 px-6">
-                    Group
-                </th>
-                <th scope="col" class="py-3 px-6">
-                    Created At
-                </th>
-                <th scope="col" class="py-3 px-6">
-                    Updated At
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple MacBook Pro 17"
-                </th>
-                <td class="py-4 px-6">
-                    Sliver
-                </td>
-                <td class="py-4 px-6">
-                    Laptop
-                </td>
-                <td class="py-4 px-6">
-                    $2999
-                </td>
-                <td class="py-4 px-6">
-                    $2999
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
 
                 </div>
             </div>
